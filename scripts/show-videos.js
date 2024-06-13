15e922511497b0c0293b8b35659e974b4a5538b4
devcomfort <kimdonghyun026@gmail.com>
2024-06-13 09:42:44 +0900

    feat : 영상 및 설명 베이스코드 추가, 초기화 로직을 개별 페이지에서 실행하도록 변경

--- scripts/show-videos.js
+++ scripts/show-videos.js
@@ -1,33 +1,123 @@
-const video_urls = [
-  "./videos/1.mp4",
-  "./videos/2.mp4",
-  "./videos/3.mp4",
-  "./videos/4.mp4",
-  "./videos/5.mp4",
-];
+/** 영상 주소들 */
+const video_urls = {
+  // 초급
+  beginner: [
+    // 걷기
+    "./videos/5.mp4",
+    // 런지
+    "./videos/6.mp4",
+    // 풀업
+    "./videos/10.mp4",
+    // 맨몸 스쿼트
+    "./videos/11.mp4",
+    // 푸쉬업
+    "./videos/12.mp4",
+  ],
+  // 중급
+  intermediate: [
+    // 바벨 런지
+    "./videos/2.mp4",
+    // 벤치프레스
+    "./videos/3.mp4",
+    // 바벨 스쿼트
+    "./videos/4.mp4",
+    // 오버헤드 프레스
+    "./videos/7.mp4",
+    // 데드리프트
+    "./videos/8.mp4",
+  ],
+  // 고급
+  advanced: [
+    // 저쳐스쿼트
+    "./videos/1.mp4",
+    // 업라이트 로우
+    "./videos/9.mp4",
+    // 백스쿼트 프레스
+    "./videos/13.mp4",
+    // 클린 앤 저크
+    "./videos/14.mp4",
+    // 링딥스
+    "./videos/15.mp4",
+  ],
+};
 
-const titles = ["초급-1", "초급-2", "초급-3", "초급-4", "초급-5"];
+/** 영상의 제목들 */
+const titles = {
+  // 초급
+  beginner: [
+    "초급 1 걷기",
+    "초급 2 런지",
+    "초급 3 풀업",
+    "초급 4 맨몸 스쿼트",
+    "초급 5 푸쉬업",
+  ],
+  // 중급
+  intermediate: [
+    "중급 1 바벨 런지",
+    "중급 2 벤치프레스",
+    "중급 3 바벨 스쿼트",
+    "중급 4 오버헤드 프레스",
+    "중급 5 데드리프트",
+  ],
+  // 고급
+  advanced: [
+    "고급 1 저쳐스쿼트",
+    "고급 2 업라이트 로우",
+    "고급 3 백스쿼트 프레스",
+    "고급 4 클린 앤 저크",
+    "고급 5 링딥스",
+  ],
+};
 
-const video_descriptions = [
-  "벤치 프레스는 웨이트 트레이닝을 해본 적이 없는 사람들도 알고 있는 경우가 많을 정도로 가장 널리 알려진 운동으로서, 대흉근과 삼두근을 위주로 가슴 부근의 상체 근육들을 종합적으로 강화하기 위한 최고의 운동이다.웨이트 트레이닝 3대 운동 중 하나답게 운동 효과가 매우 뛰어나다. 대흉근이란 이름이 붙을 만큼 거대한 가슴 근육 발달에 있어 벤치 프레스만한 운동이 없으며 가슴뿐만 아니라 어깨, 삼두 등 상체 근육 전반을 그 어떤 운동보다도 고중량으로 소화해낼 수 있는 상체 운동의 최고봉인 운동이다.",
-  "벤치 프레스는 웨이트 트레이닝을 해본 적이 없는 사람들도 알고 있는 경우가 많을 정도로 가장 널리 알려진 운동으로서, 대흉근과 삼두근을 위주로 가슴 부근의 상체 근육들을 종합적으로 강화하기 위한 최고의 운동이다.웨이트 트레이닝 3대 운동 중 하나답게 운동 효과가 매우 뛰어나다. 대흉근이란 이름이 붙을 만큼 거대한 가슴 근육 발달에 있어 벤치 프레스만한 운동이 없으며 가슴뿐만 아니라 어깨, 삼두 등 상체 근육 전반을 그 어떤 운동보다도 고중량으로 소화해낼 수 있는 상체 운동의 최고봉인 운동이다.",
-  "벤치 프레스는 웨이트 트레이닝을 해본 적이 없는 사람들도 알고 있는 경우가 많을 정도로 가장 널리 알려진 운동으로서, 대흉근과 삼두근을 위주로 가슴 부근의 상체 근육들을 종합적으로 강화하기 위한 최고의 운동이다.웨이트 트레이닝 3대 운동 중 하나답게 운동 효과가 매우 뛰어나다. 대흉근이란 이름이 붙을 만큼 거대한 가슴 근육 발달에 있어 벤치 프레스만한 운동이 없으며 가슴뿐만 아니라 어깨, 삼두 등 상체 근육 전반을 그 어떤 운동보다도 고중량으로 소화해낼 수 있는 상체 운동의 최고봉인 운동이다.",
-  "벤치 프레스는 웨이트 트레이닝을 해본 적이 없는 사람들도 알고 있는 경우가 많을 정도로 가장 널리 알려진 운동으로서, 대흉근과 삼두근을 위주로 가슴 부근의 상체 근육들을 종합적으로 강화하기 위한 최고의 운동이다.웨이트 트레이닝 3대 운동 중 하나답게 운동 효과가 매우 뛰어나다. 대흉근이란 이름이 붙을 만큼 거대한 가슴 근육 발달에 있어 벤치 프레스만한 운동이 없으며 가슴뿐만 아니라 어깨, 삼두 등 상체 근육 전반을 그 어떤 운동보다도 고중량으로 소화해낼 수 있는 상체 운동의 최고봉인 운동이다.",
-  "벤치 프레스는 웨이트 트레이닝을 해본 적이 없는 사람들도 알고 있는 경우가 많을 정도로 가장 널리 알려진 운동으로서, 대흉근과 삼두근을 위주로 가슴 부근의 상체 근육들을 종합적으로 강화하기 위한 최고의 운동이다.웨이트 트레이닝 3대 운동 중 하나답게 운동 효과가 매우 뛰어나다. 대흉근이란 이름이 붙을 만큼 거대한 가슴 근육 발달에 있어 벤치 프레스만한 운동이 없으며 가슴뿐만 아니라 어깨, 삼두 등 상체 근육 전반을 그 어떤 운동보다도 고중량으로 소화해낼 수 있는 상체 운동의 최고봉인 운동이다.",
-];
+const video_descriptions = {
+  // 초급
+  beginner: [
+    "초급 1 걷기",
+    "초급 2 런지",
+    "초급 3 풀업",
+    "초급 4 맨몸 스쿼트",
+    "초급 5 푸쉬업",
+  ],
+  // 중급
+  intermediate: [
+    "중급 1 바벨 런지",
+    "중급 2 벤치프레스",
+    "중급 3 바벨 스쿼트",
+    "중급 4 오버헤드 프레스",
+    "중급 5 데드리프트",
+  ],
+  // 고급
+  advanced: [
+    "고급 1 저쳐스쿼트",
+    "고급 2 업라이트 로우",
+    "고급 3 백스쿼트 프레스",
+    "고급 4 클린 앤 저크",
+    "고급 5 링딥스",
+  ],
+};
 
-window.addEventListener("DOMContentLoaded", function () {
-  const targetElement = document.querySelector(".videos");
+/**
+ * 영상을 표시해주는 함수
+ * @param {HTMLElement} targetElement 영상을 출력할 엘리먼트
+ * @param {"beginner" | "intermediate" | "advanced"} level
+ */
+function init(targetElement, level) {
+  targetElement ??= document.querySelector("videos");
 
-  video_urls.forEach((url, index) => {
+  video_urls[level].forEach((url, index) => {
+    /** 모든 내용을 이 엘리먼트에 담아서 출력에 사용함 */
     const newElement = document.createElement("div");
+
+    /** 제목 엘리먼트 */
+    const titleElement = document.createElement("h1");
+    /** 카드 엘리먼트; 화면에서 영상의 배경과 영상이 함께 차지하는 영역 */
     const cardElement = document.createElement("div");
+    /** 영상 엘리먼트 (카드 엘리먼트 내에 위치함) */
     const videoElement = document.createElement("video");
+    /** 영상 설명 엘리먼트 */
     const descElement = document.createElement("div");
 
-    const titleElement = document.createElement("h1");
-
-    titleElement.innerText = titles[index];
+    titleElement.innerText = titles[level][index];
 
     // 영상 주소 설정
     videoElement.setAttribute("controls", "");
@@ -35,8 +125,8 @@ window.addEventListener("DOMContentLoaded", function () {
     // 영상을 카드 엘리먼트에 입력
     cardElement.classList.add("card");
     cardElement.appendChild(videoElement);
-    // 영상 설명 설정
-    descElement.innerText = video_descriptions[index];
+    // 영상 설명 데이터를 가져와 입력함
+    descElement.innerText = video_descriptions[level][index];
 
     // 영상 카드와 영상 설명을 엘리먼트에 추가
     newElement.appendChild(titleElement);
@@ -46,4 +136,4 @@ window.addEventListener("DOMContentLoaded", function () {
     // 최종적으로 영상 목록 엘리먼트에 추가
     targetElement.appendChild(newElement);
   });
-});
+}
